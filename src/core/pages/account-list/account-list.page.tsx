e01fb52703d0b4638831bf6051da5b4c2af3ff4e
Jordi Portell <jordiportellmas@gmail.com>
2024-11-30 15:21:42 +0100

    Routes

---
+++ src/core/pages/account-list/account-list.page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { Link, generatePath } from "react-router-dom";
+import { appRoutes } from "@/core/router";
+
+export const AccountListPage: React.FC = () => {
+    return <div>Account List
+        <br />
+        <Link to={generatePath(appRoutes.movements, { id: 1 })}>
+            Movimientos de cuenta 1
+        </Link>
+        <br />
+        <Link to={appRoutes.transfer}>Transferencia</Link>
+        <br />
+        <Link to={generatePath(appRoutes.transferFromAccount, { id: 1 })}>
+            Transferencia desde cuenta 1
+        </Link>
+        <br />
+    </div>;
+};
